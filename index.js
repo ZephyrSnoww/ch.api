bd66afc00c1c7a17d732b3f154d95e967c478a15
Sylas <38060892+glacierheavner@users.noreply.github.com>
2021-12-19 13:10:26 -0700

    Added user normalizing

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ exports.NormalizeChannel = require("./util/NormalizeChannel");
 exports.NormalizeMember = require("./util/NormalizeMember");
 exports.NormalizeMessage = require("./util/NormalizeMessage");
 exports.NormalizeServer = require("./util/NormalizeServer");
+exports.NormalizeUser = require("./util/NormalizeUser");
+
 exports.discordIntents = Discord.Intents;
--- util/NormalizeUser.js
+++ util/NormalizeUser.js
@@ -5,5 +5,47 @@
  * @returns {Object}
  */
 exports.NormalizeUser = (user, client) => {
+    let normalizedUser = {
+        avatar: null,
+        banner: null,
+        bot: false,
+        client: client.client,
+        createdAt: new Date(),
+        discriminator: null,
+        id: 0,
+        username: null,
+        originalUser: user
+    };
+
+    if (client.name === "Telegram") {
+        normalizedUser.avatar = null; // TODO
+        normalizedUser.banner = null; // TODO
+        normalizedUser.bot = user.is_bot;
+        normalizedUser.createdAt = null; // TODO
+        normalizedUser.discriminator = null; // TODO?
+        normalizedUser.id = user.id;
+        normalizedUser.username = user.username;
+    }
+
+    if (client.name === "Discord") {
+        normalizedUser.avatar = user.avatarURL();
+        normalizedUser.banner = user.bannerURL();
+        normalizedUser.bot = user.bot;
+        normalizedUser.createdAt = user.createdAt;
+        normalizedUser.discriminator = user.discriminator;
+        normalizedUser.id = user.id;
+        normalizedUser.username = user.username;
+    }
+
+    if (client.name === "Revolt") {
+        normalizedUser.avatar = user.generateAvatarURL();
+        normalizedUser.banner = user.fetchProfile();
+        normalizedUser.bot = user.bot;
+        normalizedUser.createdAt = user.createdAt;
+        normalizedUser.discriminator = null; // TODO?
+        normalizedUser.id = user._id;
+        normalizedUser.username = user.username;
+    }
+
     return normalizedUser;
 }
